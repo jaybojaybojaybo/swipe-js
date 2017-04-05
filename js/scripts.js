1f2d2bcd0ab759b22408a4a60ccec8f41cf24328
jaybojaybojaybo <jasun.feddema@gmail.com>
2017-04-05 00:40:27 -0700

    create js file for IAHWOS game.

---
+++ js/scripts.js
@@ -0,0 +1,46 @@
+function Story(character,date,breakfast,lunch,dinner,drinks,decision) {
+  this.character = plot;
+  this.date = date;
+  this.breakfast = breakfast;
+  this.lunch = lunch;
+  this.dinner = dinner;
+  this.drinks = drinks;
+  this.decision = decision;
+}
+
+
+
+
+
+var Pat = {
+  book:"In Cold Blood - Truman Capote",
+  music:"The Misfits",
+  movie:"Rear Window",
+  philosophy:"Nihilism",
+  date-preference:bookstore,
+
+}
+
+var Chris = {
+  book:"Cosmos by Carl Sagan"
+  music:"Bjork"
+  movie:"The Last Starfighter... wink wink!"
+  philosophy:"Jedi...ism"
+  date-preference:theatre,
+}
+
+var Sam = {
+  book:"A Brief History of Time by Stephen Hawking";
+  music:"The Futureheads";
+  movie:"Back to the Future";
+  philosophy:"Altruism";
+  date-preference:show;
+}
+
+var Jesse = {
+  book:"Not really into books; I read news headlines, tho!"
+  music:"Drake! You know when that hotline bling!..."
+  movie:"Mike and Dave need Wedding Dates"
+  philosophy:"YOLO"
+  date-preference:lecture,
+}
